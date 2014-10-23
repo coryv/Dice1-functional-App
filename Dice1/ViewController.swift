e895e89da9c406275a0a7e1a66efb3b0bf674380
coryv <coryvogan@gmail.com>
2014-10-23 11:49:32 -0400

    Initial Commit

---
+++ Dice1/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Dice1
+//
+//  Created by Cory Vogan on 10/23/14.
+//  Copyright (c) 2014 Cory Vogan. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
